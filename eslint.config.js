73ad4c49c29dc7b91e3859302db62fcadfb9dc98
Rômulo Pinheiro <romulopinheirocosta@gmail.com>
2026-07-29 17:07:47 -0300

    build(eslint): configura ESLint e adiciona comandos de validação no Makefile

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  ...eslintPluginAstro.configs.recommended,
+
+  // Configurações comuns para todo o projeto
+  {
+    ignores: [
+        ".astro/",
+        "dist/",
+        "node_modules"
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  // Espaço reservado para regras específicas do projeto
+  {
+    rules: {
+      // Exemplo:
+      // "astro/no-set-html-directive": "error",
+    },
+  },
+];
